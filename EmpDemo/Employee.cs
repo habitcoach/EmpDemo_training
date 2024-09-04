f05075f390cb12e94d17200662239bc3d1271858
habitcoach <the_habit_coach@outlook.com>
2024-09-04 10:54:37 +0400

    Add project files.

---
+++ EmpDemo/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpDemo
+{
+    public class Employee
+    {
+        // Prop
+
+        
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public int Phone { get; set; }
+        public string? Title { get; set; }
+
+
+        
+
+    }
+}
---
+++ EmpDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace EmpDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Employee AliEmp = new Employee();
+
+            AliEmp.Id = 1;
+            AliEmp.Name = "Ali something";
+            AliEmp.Address = "Oman";
+            AliEmp.Title = "Manager";
+            AliEmp.Phone = 99999999;
+
+            Console.WriteLine("The emp detials are: ");
+
+            Console.WriteLine(AliEmp.Name + "\n" + AliEmp.Id +"\n"+ AliEmp.Phone);
+
+
+        }
+
+        
+    }
+    // <class name> <obj name> = new <class name>();
+    
+}
